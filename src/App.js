338b42d00b42bff14597976dc0606a7bdf2c8faa
Aman <amanmankoo96@gmail.com>
2020-07-27 14:17:37 +0530

    initialised the cra, installed some packages, navbar is added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavBar from './components/NavBar';
+import { Container } from 'semantic-ui-react';
+
+const App = () => {
+  return (
+    <Container>
+      <NavBar />
+    </Container>
+  );
+};
+
+export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Menu, Icon } from 'semantic-ui-react';
+
+const NavBar = () => {
+  return (
+    <Menu secondary color="teal">
+      <Menu.Item header>
+        <Icon name="search" />
+        Profile Finder
+      </Menu.Item>
+      <Menu.Item as="a" name="Home" active />
+      <Menu.Item as="a" name="About" />
+    </Menu>
+  );
+};
+
+export default NavBar;
